fdd891087f110d29f2028e82dae4e39fe1c81927
aracnathea66 <aracnathea66@gmail.com>
2020-12-06 13:43:48 +0100

    m5-solution

--- m5-solution/js/script.js
+++ m5-solution/js/script.js
@@ -82,8 +82,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  allCategoriesUrl,
-  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
+  allCategoriesUrl, 
+  buildAndShowHomeHTML,
+
+    // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -91,17 +93,23 @@ $ajaxUtils.sendGetRequest(
 
 // Builds HTML for the home page based on categories array
 // returned from the server.
-function buildAndShowHomeHTML (categories) {
+function buildAndShowHomeHTML(categories) {
+
+  // body...
 
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
-    function (homeHtml) {
+    function (homeHtmlUrl) {
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+
+ var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+
+
+    
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -115,17 +123,26 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+  // chosenCategoryShortName="'"+chosenCategoryShortName+"'";
+  var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", "'"+chosenCategoryShortName+"'");
+
+
+  // var insertProperty = function (homeHtmlUrl, randomCategoryShortName, chosenCategoryShortName) {
+  //   var propToReplace = "{{" + randomCategoryShortName + "}}";
+  //   string = string
+  //   .replace(new RegExp(propToReplace, "g"), chosenCategoryShortName);
+  //   return string;
+  // }
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-
+  insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
-}
+    }
 
 
 // Given array of category objects, returns a random category object.
@@ -341,3 +358,6 @@ function insertItemPortionName(html,
 global.$dc = dc;
 
 })(window);
+
+
+
